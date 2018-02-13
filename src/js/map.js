26cc269329d598beb5f7d82b5bf4fb36873711e0
Oleksii Svirzhevskyi <o.svirzhevskyi@andersenlab.com>
2018-02-13 16:59:25 +0200

    fixed order buttons fixed map size

--- src/js/map.js
+++ src/js/map.js
@@ -1,20 +1,20 @@
 ymaps.ready(function () {
     let myMap = new ymaps.Map('map-widget', {
-            center: [60.000000, 30.299382],
-            zoom: 12,
+            center: [59.934724, 30.334743],
+            zoom: 11,
             controls: []
         }, {
             searchControlProvider: 'yandex#search'
         });
 
     let iconLayout = ymaps.templateLayoutFactory.createClass(
-            '<svg class="svg-icon svg-icon-placemark map__placemark map__placemark--{{ properties.color }}">' +
+            '<svg class="svg-icon svg-icon-placemark map__placemark map__placemark_{{ properties.color }}">' +
             '    <use xlink:href="#placemark"></use>' +
             '</svg>'
         );
     ymaps.layout.storage.add('my#newPlacemark', iconLayout);
 
-    let placemark1 = new ymaps.Placemark([60.000000, 30.299382], {
+    let placemark1 = new ymaps.Placemark([59.971131, 30.340544], {
             hintContent: 'Собственный значок метки с контентом',
             color: 'red'
         }, {
@@ -23,18 +23,35 @@ ymaps.ready(function () {
             iconLayout: 'my#newPlacemark'
         }),
 
-        placemark2 = new ymaps.Placemark([60.010000, 30.299382], {
+        placemark2 = new ymaps.Placemark([60.001837, 30.385176], {
             hintContent: 'Собственный значок метки с контентом',
             color: 'green'
         }, {
             // Опции.
             // Необходимо указать данный тип макета.
             iconLayout: 'my#newPlacemark'
+        }),
+        placemark3 = new ymaps.Placemark([60.000977, 30.294883], {
+            hintContent: 'Собственный значок метки с контентом',
+            color: 'red'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'my#newPlacemark'
+        }),
+        placemark4 = new ymaps.Placemark([59.906532, 30.300003], {
+            hintContent: 'Собственный значок метки с контентом',
+            color: 'red'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'my#newPlacemark'
         });
-
     myMap.geoObjects
         .add(placemark1)
-        .add(placemark2);
+        .add(placemark2)
+        .add(placemark3)
+        .add(placemark4);
 });
 
 $(document).ready(function () {
