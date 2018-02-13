d58ea9f4c9b7715917b1a6ecffce6d4ba41950bd
Oleksii Svirzhevskyi <o.svirzhevskyi@andersenlab.com>
2018-02-13 16:59:34 +0300

    made slider

--- src/js/order-item.js
+++ src/js/order-item.js
@@ -1,6 +1,6 @@
 (function($) {
   $(document).ready(function() {
-    let orderItemExpander = $('.js-order-item-expander');
+    var orderItemExpander = $('.js-order-item-expander');
 
     orderItemExpander.on('click', function() {
       $(this).parents('.js-order-item').toggleClass('is-expanded');
